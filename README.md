53b1fc5761aabc80759fee8c9f51822f7055dc47
Josh Taylor <jtaylor9110@gmail.com>
2016-11-26 21:20:21 -0500

    updated colorcheme to match terminal theme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## A Simple Startpage Built to enhance my Firefox Experience##
 
-![FF Home](/img/ff-home-2016-10-09.png)
+![FF Home](/img/ff-home-20161126.png)
 
 Recently, I have been a little annoyed with the directions in which Mozilla have been taking Firefox (among other things, the new tab page). As such, I have gone ahead and created a new tab page that has things available that I use and is a little more pleasant to look at IMO (because aesthetics trump usability, right?)
 
--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
 function pad(n){
-  return (n < 10) ? '0' + n : n;
+	return (n < 10) ? '0' + n : n;
 }
 
 function timer(){
-  var now   = new Date,
-      // Math, yo. If hrs divides cleanly by 12, that is the hr. Otherwise, 12
-      hrs   = now.getHours() % 12 || 12,
-      min   = now.getMinutes(),
-      sec   = now.getSeconds(),
-  timestr   = [ pad(hrs), (pad(min)), (pad(sec)) ].join(':');
+	var now   = new Date,
+	// Math, yo. If hrs divides cleanly by 12, that is the hr. Otherwise, 12
+		hrs   = now.getHours() % 12 || 12,
+		min   = now.getMinutes(),
+		sec   = now.getSeconds(),
+		timestr   = [ pad(hrs), (pad(min)), (pad(sec)) ].join(':');
 
-  document.getElementById('time').innerHTML = timestr;
-  setTimeout(timer, 1000);
+	document.getElementById('time').innerHTML = timestr;
+	setTimeout(timer, 1000);
 }
 
 function getDate(){
-  var today = new Date,
-      dd    = today.getDate(),
-      mm    = today.getMonth() + 1,
-      yyyy  = today.getFullYear();
+	var today = new Date,
+		dd    = today.getDate(),
+		mm    = today.getMonth() + 1,
+		yyyy  = today.getFullYear();
 
-  today = [pad(mm), pad(dd), yyyy].join('-');
-  document.getElementById('date').innerHTML = today;
+	today = [pad(mm), pad(dd), yyyy].join('-');
+	document.getElementById('date').innerHTML = today;
 }
 
 window.onload = function(){
-  timer();
-  getDate();
-}
+	timer();
+	getDate();
+};
+
